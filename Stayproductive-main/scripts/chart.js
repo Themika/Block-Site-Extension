401bae0101e0841b761d21099e600097fa5ce18d
Themiika <126718397+Themiika@users.noreply.github.com>
2024-01-25 22:25:16 -0500

    Install Chart.js

--- Stayproductive-main/scripts/chart.js
+++ Stayproductive-main/scripts/chart.js
@@ -1,24 +1,22 @@
-// // Check loading
-// console.log("d3Script is created");
+const ctx = document.getElementById("myChart");
 
-// const d3Script = document.createElement("script");
-// d3Script.src = chrome.runtime.getURL("scripts/d3.v7.min.js");
-// document.head.appendChild(d3Script);
-// // Event listener for script load
-// d3Script.onload = function () {
-//   console.log("d3Script is loaded");
-//   // Example: Append an SVG element to the body
-//   //hhh
-//   const svg = d3
-//     .select("body")
-//     .append("svg")
-//     .attr("width", 500)
-//     .attr("height", 500);
-
-//   svg
-//     .append("circle")
-//     .attr("cx", 250)
-//     .attr("cy", 250)
-//     .attr("r", 50)
-//     .style("fill", "red");
-// };
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
